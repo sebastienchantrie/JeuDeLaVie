7baa6a48f5f687704fd2be90071f05a97c264c8e
sebastienchantrie <sebastien.chantrie@gmail.com>
2019-09-26 19:18:38 +0200

    norme

--- script.js
+++ script.js
@@ -13,39 +13,33 @@ function generate_table() {
         let row = document.createElement("tr");
     for (let j = 0; j < nbtab; j++) {
         let cell = document.createElement("td");
-        if(nbtab === 32) {
+        if (nbtab === 32) {
             cell.classList.add('mini');
             body.classList.add('col-lg-8');
             body.classList.add('littlemargin');
-
-        }else if(nbtab === 64) {
+        } else if (nbtab === 64) {
             cell.classList.add('middle');
             body.classList.add('col-lg-6');
             body.classList.add('middlemargin');
-        }else {
+        } else {
             cell.classList.add("big");
             body.classList.add('col-lg-8');
             body.classList.add('bigmargin');
         }
         row.appendChild(cell);
-    
     }
     tblBody.appendChild(row);
     let td = document.getElementsByTagName('td');
-    console.log(td)
     for (let n = 0 ; n < td.length ; n++) {
-    td[n].addEventListener('click' , (e) => {
-        td[n].style.backgroundColor = "black";
-    })
-}
+        td[n].addEventListener('click' , (e) => {
+            td[n].style.backgroundColor = "black";
+        })
+    }
     let move = document.getElementById('move');
     move.classList.add('move');
-
     tbl.appendChild(tblBody);
     body.appendChild(tbl);
     tbl.setAttribute("border", "2");
     }
 }
-
-
     // rester appuyer sur la souris et modifier
